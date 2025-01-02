ed9962ed5f12a1b07e9d5fae0e9b392c587dbe42
dhrkawk <jajwon202@naver.com>
2025-01-03 02:13:04 +0900

    step8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,24 @@
+def brGage(num, current_player):
+    try:
+        count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+        if count not in [1, 2, 3]:
+            print("1,2,3 중 하나를 입력하세요")
+            return num, False
+    except ValueError:
+        print("정수를 입력하세요")
+        return num, False
+    
+    for i in range(1, count + 1):
+        num += 1
+        print(f"{current_player}: ", num)
+        if num >= 31:
+            return num, True
+    
+    return num, False
+
+
+#코드 블록 자체의 중복성이 아닌 코드의 논리적 흐름 중 재사용성에 입각하여 중복 코드 추출
+
 num = 0
 players = ["playerA", "playerB"]
 turn = 0
@@ -5,18 +26,11 @@ turn = 0
 while True:
     current_player = players[turn]
     no_current_player = players[1 - turn]
-    try:
-        count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-        if count not in [1, 2, 3]:
-            print("1,2,3 중 하나를 입력하세요")
-            continue
-    except ValueError:
-        print("정수를 입력하세요")
-    
-    for i in range(1, count + 1):
-        num += 1
-        print(f"{current_player}: ", num)
-        if num >= 31:
-            print(f"{no_current_player} win!")
-            exit()
+    num, is_over = brGage(num, current_player)
+
+    if is_over:
+        print(f"{no_current_player} win!")
+        break
+
     turn = 1 - turn
+    
